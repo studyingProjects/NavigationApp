6c31e23561ba676ba22742651d4347acc175674e
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-02-13 14:34:45 +0300

    add navigation push and pop methods

--- NavigationApp/ViewController.swift
+++ NavigationApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // link to storyboard
+    let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func toGreenScene(_ sender: UIButton) {
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "greenViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    @IBAction func toYellowScene(_ sender: UIButton) {
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "yellowViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+        
+    }
+    
+    @IBAction func toRootScene(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func toPreviousScene(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 
 }
 
