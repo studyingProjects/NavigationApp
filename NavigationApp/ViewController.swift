5a32a22da51fb4605c20407323b2caceef5a67ae
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-02-14 12:46:00 +0300

    working with NC stack examples

--- NavigationApp/ViewController.swift
+++ NavigationApp/ViewController.swift
@@ -35,6 +35,25 @@ class ViewController: UIViewController {
     @IBAction func toPreviousScene(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
+
+    
+    func NavigationOptions() {
+        // 1.Возврат к определенной сцене
+        // перебираем все элементы стека
+        self.navigationController?.viewControllers.forEach { viewController in
+            // определяем требуемый контроллер
+            // ViewController - проверяем на тип, здесь может быть указан любой VC любой сцены
+            if viewController is ViewController {
+                // производим возврат к нему
+                self.navigationController?.popToViewController(viewController, animated: true)
+            }
+        }
+        
+        // 2. Работа с корневой сценой // изменяем корневую сцену
+        let someViewController = ViewController()
+        self.navigationController?.viewControllers[0] = someViewController
+
+    }
     
 
 }
